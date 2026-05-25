bee04acf84c520066cb4d6283de0163b15a6fae5
asvincov <asvincov@gmail.com>
2026-05-25 09:35:17 +0300

    feat: hero image, stats block, nav subtitles, fixed call button

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js}'],
+  content: ['./src/**/*.{astro,html,js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
